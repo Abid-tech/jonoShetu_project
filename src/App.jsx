b0927c2b1f1229ccacb69bf145f3deb411822a95
Audrija Chowdhury <chowdhuryaudrija@gmail.com>
2026-02-28 23:25:13 +0600

    registration page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Registration from '../src/components/registration'
 
 function App() {
   
   return (
     <>
-      
+      <Registration/>
     </>
   )
 }
