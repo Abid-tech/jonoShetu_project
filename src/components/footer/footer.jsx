a6dfbfc66753d5b8569f12ca3c133ec026076971
unknown <abid0001589@gmail.com>
2026-03-02 23:04:34 +0600

    complete merged

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+
+  return (
+    <>
+      <section id="footer" className="text-white text-center py-3 mt-auto">
+            <div className="container">
+                <div className="footer-body">
+                    <h3 className="mb-0">কপিরাইট © ২০২৬ সর্বস্বত্ব সংরক্ষিত
+    গণপ্রজাতন্ত্রী বাংলাদেশ সরকার</h3>
+                </div>
+            </div>
+     </section>
+
+    </>
+  )
+}
+
+export default Footer
