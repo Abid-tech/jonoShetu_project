b9af6cd3bae82074907d2907138ef279d12b827c
unknown <abid0001589@gmail.com>
2026-04-25 13:31:37 +0600

    Additional changes

--- backend/routes/registration.js
+++ backend/routes/registration.js
@@ -1,11 +1,7 @@
 const express    = require("express");
 const router     = express.Router();
-<<<<<<< HEAD
 const path = require("path");
 const upload = require(path.join(__dirname, "../middleware/upload"));
-=======
-const upload     = require("../middleware/upload");
->>>>>>> e317dc5d1e4f0752ec699b1d44d1d835c39860b6
 const { registerUser, checkNID } = require("../controller/registrationController");
 
 // POST /register
